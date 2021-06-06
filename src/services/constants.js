06bec5f3d7219a5bcae5eda8474c27ae5e8a3cb4
AlphaCodes <developers@ewallet.co.tz>
2021-06-06 21:07:04 +0300

    updated configurations

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,3 +1,5 @@
 // export const API_URL = "http://localhost:8088/api/v1"
 
-export const API_URL = "http://10.154.11.117:8088/api/v1"
+// export const API_URL = "http://10.154.11.117:8088/api/v1"
+
+export const API_URL = "http://197.250.3.246:8085/api/v1"
