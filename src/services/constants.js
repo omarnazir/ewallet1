479ed033528f3c1f65b9eaca3edfeb1900e589d6
AlphaCodes <developers@ewallet.co.tz>
2021-07-23 16:39:46 +0300

    update api url

--- src/services/constants.js
+++ src/services/constants.js
@@ -2,6 +2,6 @@
 
 // export const API_URL = "http://10.154.11.117:8088/api/v1"
 
-export const API_URL = "http://197.250.3.246:8085/api/v1"
+// export const API_URL = "http://197.250.3.246:8085/api/v1"
 
-
+export const API_URL = "https://sms.vodacom.co.tz/api/v1/"
