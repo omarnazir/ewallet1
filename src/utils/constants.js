de8d270615a50896f4506218f9140f9aa7d81a1e
Ewallet Developers Team <developers@ewallet.co.tz>
2021-11-16 18:56:09 +0000

    Update src/utils/constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 // export const API_URL = "http://localhost:8082/api/v1"
 
 // export const API_URL="https://mkulima.vodacom-dev.com/api/v1";
-export const API_URL = "http://10.154.11.117:8081/api/v1";
+export const API_URL = "http://10.154.11.117:8085/api/v1";
 
 
